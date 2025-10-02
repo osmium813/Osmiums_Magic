284fe8d031d3e743fe365c4392f14586edbd2946
osmium813 <tungstensteel1168@gmail.com>
2025-10-02 18:41:25 +0900

    a

--- src/main/java/com/Osmium/OsmiumsMagic/Main/Osmiumsmagic.java
+++ src/main/java/com/Osmium/OsmiumsMagic/Main/Osmiumsmagic.java
@@ -1,11 +1,16 @@
 package com.Osmium.OsmiumsMagic.Main;
 
 import com.Osmium.OsmiumsMagic.inventory.menu.ModMenuTypes;
+import com.Osmium.OsmiumsMagic.inventory.screen.DoubleRingScreen;
 import com.Osmium.OsmiumsMagic.regi.ModBlocks;
 import com.Osmium.OsmiumsMagic.regi.ModItems;
 import com.Osmium.OsmiumsMagic.regi.tab.OsmiumsmagicTabs;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("osmiumsmagic")
@@ -22,5 +27,13 @@ public class Osmiumsmagic {
         OsmiumsmagicTabs.MOD_TABS.register(bus);
 
         ModMenuTypes.resister(bus);
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            MenuScreens.register(ModMenuTypes.DOUBLE_RING_MENU.get(), DoubleRingScreen::new);
+        }
     }
 }
