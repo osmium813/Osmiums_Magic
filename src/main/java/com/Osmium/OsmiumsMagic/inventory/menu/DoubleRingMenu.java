7b09741a14bf68c312ad414dabf84db24b314c4b
osmium813 <tungstensteel1168@gmail.com>
2025-10-02 20:02:19 +0900

    a

--- src/main/java/com/Osmium/OsmiumsMagic/inventory/menu/DoubleRingMenu.java
+++ src/main/java/com/Osmium/OsmiumsMagic/inventory/menu/DoubleRingMenu.java
@@ -21,11 +21,11 @@ public class DoubleRingMenu extends AbstractContainerMenu {
 
         for (int row = 0; row < 3; ++row) {
             for (int col = 0; col < 9; ++col) {
-                this.addSlot(new Slot(inventory, col + row * 9 + 9, 8 + col * 18, 84 + row * 18));
+                this.addSlot(new Slot(inventory, col + row * 9 + 9, 8 + col * 18, 51 + row * 18));
             }
         }
         for (int col = 0; col < 9; ++col) {
-            this.addSlot(new Slot(inventory, col, 8 + col * 18, 142));
+            this.addSlot(new Slot(inventory, col, 8 + col * 18, 109));
         }
     }
 
