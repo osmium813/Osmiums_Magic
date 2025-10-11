6ff218e23231ada16e0b5198d683fa3340081366
osmium813 <tungstensteel1168@gmail.com>
2025-10-11 14:09:21 +0900

    a

--- src/main/java/com/Osmium/OsmiumsMagic/datagen/ModItemModelProvider.java
+++ src/main/java/com/Osmium/OsmiumsMagic/datagen/ModItemModelProvider.java
@@ -2,7 +2,6 @@ package com.Osmium.OsmiumsMagic.datagen;
 
 import com.Osmium.OsmiumsMagic.Main.Osmiumsmagic;
 import com.Osmium.OsmiumsMagic.regi.ModItems;
-import io.redspace.ironsspellbooks.item.curios.CurioBaseItem;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -28,29 +27,29 @@ public class ModItemModelProvider extends ItemModelProvider {
         curioItem(ModItems.COOLDOWN_RING_TIER_THREE);
         curioItem(ModItems.COOLDOWN_RING_TIER_FOUR);
         curioItem(ModItems.COOLDOWN_RING_TIER_FIVE);
-        curioItem(ModItems.CAST_TIME_RING_TIER_ONE);
-        curioItem(ModItems.CAST_TIME_RING_TIER_TWO);
-        curioItem(ModItems.CAST_TIME_RING_TIER_THREE);
-        curioItem(ModItems.CAST_TIME_RING_TIER_FOUR);
-        curioItem(ModItems.CAST_TIME_RING_TIER_FIVE);
+        //curioItem(ModItems.CAST_TIME_RING_TIER_ONE);
+        //curioItem(ModItems.CAST_TIME_RING_TIER_TWO);
+        //curioItem(ModItems.CAST_TIME_RING_TIER_THREE);
+        //curioItem(ModItems.CAST_TIME_RING_TIER_FOUR);
+        //curioItem(ModItems.CAST_TIME_RING_TIER_FIVE);
         curioItem(ModItems.MANA_COOLDOWN_RING);
-        curioItem(ModItems.MANA_COOLDOWN_RING_TIER_ONE);
-        curioItem(ModItems.MANA_COOLDOWN_RING_TIER_TWO);
-        curioItem(ModItems.MANA_COOLDOWN_RING_TIER_THREE);
-        curioItem(ModItems.MANA_COOLDOWN_RING_TIER_FOUR);
-        curioItem(ModItems.MANA_COOLDOWN_RING_TIER_FIVE);
-        curioItem(ModItems.MANA_CAST_TIME_RING);
-        curioItem(ModItems.MANA_CAST_TIME_RING_TIER_ONE);
-        curioItem(ModItems.MANA_CAST_TIME_RING_TIER_TWO);
-        curioItem(ModItems.MANA_CAST_TIME_RING_TIER_THREE);
-        curioItem(ModItems.MANA_CAST_TIME_RING_TIER_FOUR);
-        curioItem(ModItems.MANA_CAST_TIME_RING_TIER_FIVE);
-        curioItem(ModItems.COOLDOWN_CAST_TIME_RING);
-        curioItem(ModItems.COOLDOWN_CAST_TIME_RING_TIER_ONE);
-        curioItem(ModItems.COOLDOWN_CAST_TIME_RING_TIER_TWO);
-        curioItem(ModItems.COOLDOWN_CAST_TIME_RING_TIER_THREE);
-        curioItem(ModItems.COOLDOWN_CAST_TIME_RING_TIER_FOUR);
-        curioItem(ModItems.COOLDOWN_CAST_TIME_RING_TIER_FIVE);
+        //curioItem(ModItems.MANA_COOLDOWN_RING_TIER_ONE);
+        //curioItem(ModItems.MANA_COOLDOWN_RING_TIER_TWO);
+        //curioItem(ModItems.MANA_COOLDOWN_RING_TIER_THREE);
+        //curioItem(ModItems.MANA_COOLDOWN_RING_TIER_FOUR);
+        //curioItem(ModItems.MANA_COOLDOWN_RING_TIER_FIVE);
+        //curioItem(ModItems.MANA_CAST_TIME_RING);
+        //curioItem(ModItems.MANA_CAST_TIME_RING_TIER_ONE);
+        //curioItem(ModItems.MANA_CAST_TIME_RING_TIER_TWO);
+        //curioItem(ModItems.MANA_CAST_TIME_RING_TIER_THREE);
+        //curioItem(ModItems.MANA_CAST_TIME_RING_TIER_FOUR);
+        //curioItem(ModItems.MANA_CAST_TIME_RING_TIER_FIVE);
+        //curioItem(ModItems.COOLDOWN_CAST_TIME_RING);
+        //curioItem(ModItems.COOLDOWN_CAST_TIME_RING_TIER_ONE);
+        //curioItem(ModItems.COOLDOWN_CAST_TIME_RING_TIER_TWO);
+        //curioItem(ModItems.COOLDOWN_CAST_TIME_RING_TIER_THREE);
+        //curioItem(ModItems.COOLDOWN_CAST_TIME_RING_TIER_FOUR);
+        //curioItem(ModItems.COOLDOWN_CAST_TIME_RING_TIER_FIVE);
     }
 
     private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
@@ -59,7 +58,7 @@ public class ModItemModelProvider extends ItemModelProvider {
                 new ResourceLocation(Osmiumsmagic.MOD_ID, "item/" + item.getId().getPath()));
     }
 
-    private ItemModelBuilder curioItem(RegistryObject<CurioBaseItem> item) {
+    private ItemModelBuilder curioItem(RegistryObject<? extends Item> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
                 new ResourceLocation(Osmiumsmagic.MOD_ID, "item/" + item.getId().getPath()));
